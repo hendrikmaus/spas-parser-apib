39cba446cbf6c760585e7041ef6bf25121ae0922
Hendrik Maus <aidentailor@gmail.com>
2016-09-01 11:17:13 +0200

    Add a test and a travis config

---
+++ tests/ApibParsedRequestsProviderTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Hmaus\Spas\Parser\Apib\Tests;
+
+use Hmaus\Spas\Parser\Apib\ApibParsedRequestsProvider;
+use Hmaus\SpasParser\SpasResponse;
+use Symfony\Component\HttpFoundation\HeaderBag;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+class ApibParsedRequestsProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testProvider()
+    {
+        $fixture = json_decode(
+            file_get_contents(__DIR__ . '/fixtures/09. Advanced Attributes.md.refract.json'), true
+        );
+
+        $provider = new ApibParsedRequestsProvider();
+        $parsedRequests = $provider->parse($fixture);
+
+        $this->assertCount(3, $parsedRequests);
+
+        foreach ($parsedRequests as $request) {
+            $this->assertInstanceOf(ParameterBag::class, $request->getParams());
+            $this->assertInstanceOf(HeaderBag::class, $request->getHeaders());
+            $this->assertNotEmpty($request->getName());
+            $this->assertEmpty($request->getBaseUrl());
+            $this->assertNotEmpty($request->getHref());
+            $this->assertInternalType('string', $request->getContent());
+            $this->assertNotEmpty($request->getMethod());
+            $this->assertTrue($request->isEnabled());
+            $this->assertInstanceOf(SpasResponse::class, $request->getResponse());
+
+            $response = $request->getResponse();
+            $this->assertInstanceOf(HeaderBag::class, $response->getHeaders());
+            $this->assertNotEmpty($response->getSchema());
+            $this->assertNotEmpty($response->getStatusCode());
+            $this->assertNotEmpty($response->getBody());
+        }
+    }
+}
